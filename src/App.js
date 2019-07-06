c6bcb12041fe7aaacfd8d99c6e7454e0201bcb84
Noboru Yamaguchi <noboru.libra@gmail.com>
2019-07-06 14:07:53 +0900

    TextField追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import TextField from "@material-ui/core/TextField";
+
+const defaultText = "Hello, world!";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <TextField defaultValue={defaultText} /> 
+      </div>
     </div>
   );
 }
